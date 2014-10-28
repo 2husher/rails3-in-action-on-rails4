88bc0bd297904b6c11ef8f66d421a4dad07e76eb
Alex Izotov <martinandrudy@gmail.com>
2014-10-28 18:28:53 +0300

    added CRUD for tickets
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,55 @@
+class TicketsController < ApplicationController
+    before_filter :find_project
+    before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+
+
+    def new
+        @ticket = @project.tickets.build
+    end
+
+    def create
+        @ticket = @project.tickets.build(ticket_params)
+        if @ticket.save
+            flash[:notice] = "Ticket has been created."
+            redirect_to [@project, @ticket]
+        else
+            flash[:alert] = "Ticket has not been created."
+            render action: "new"
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @ticket.update_attributes(ticket_params)
+            flash[:notice] = "Ticket has been updated."
+            redirect_to [@project, @ticket]
+        else
+            flash[:alert] = "Ticket has not been updated."
+            render :action => "edit"
+        end
+    end
+
+    def destroy
+        @ticket.destroy
+        flash[:notice] = "Ticket has been deleted."
+        redirect_to @project
+    end
+
+    private
+        def find_project
+            @project = Project.find(params[:project_id])
+        end
+
+        def set_ticket
+            @ticket = @project.tickets.find(params[:id])
+        end
+
+        def ticket_params
+            params.require(:ticket).permit(:title, :description)
+        end
+end
